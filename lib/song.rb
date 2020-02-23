80d8095d19d805daddacedd76ac4d820ca20d7c3
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-02-23 02:22:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+def self.create 
+    song = self.new
+    song.save
+    song
+  end  
+  
+ def self.new_by_name(name) 
+  song = self.new 
+  song.name = name 
+  song 
+ end 
+  
+def self.create_by_name(name)
+  song = self.create 
+  song.name = name 
+  song
+end 
+  
+def self.find_by_name(song_name)
+  @@all.find {|song| song.name == song_name}
+end 
+
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) or self.create_by_name(name)
+end   
+
+def self.alphabetical 
+  @@all.sort_by{|name| name.name} 
+end   
+
+def self.new_from_filename(filename)
+info = filename.split(" - ")
+
+song = self.new 
+song.name = info[1].chomp(".mp3")
+song.artist_name = info[0]
+song 
+end 
+
+def self.create_from_filename(filename)
+info = filename.split(" - ")
+
+song = self.create 
+song.name = info[1].chomp(".mp3")
+song.artist_name = info[0]
+song 
+end 
+
+
+
+
 end
