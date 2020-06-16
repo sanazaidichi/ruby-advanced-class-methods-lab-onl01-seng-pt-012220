1cb9de930b88b3cf9b2b6b7b7efddf236cccb95d
sanazaidichi <sana.rizvi.5@gmail.com>
2020-06-16 18:55:33 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,30 +6,22 @@ class Song
     @@all
   end
 
-  def save
-    self.class.all << self
-  end
+  def self.create 
+    song = self.new.save
+  end 
 
-def self.create 
-    song = self.new
-    song.save
-    song
-  end  
-  
- def self.new_by_name(name) 
+def self.new_by_name(name) 
   song = self.new 
   song.name = name 
   song 
  end 
-  
+
 def self.create_by_name(name)
-  song = self.create 
-  song.name = name 
-  song
+  song = Song.new_by_name(name).save
 end 
   
-def self.find_by_name(song_name)
-  @@all.find {|song| song.name == song_name}
+def self.find_by_name(name)
+self.all.find{|song| song.name ==name}
 end 
 
 def self.find_or_create_by_name(name)
@@ -41,7 +33,8 @@ def self.alphabetical
 end   
 
 def self.new_from_filename(filename)
-info = filename.split(" - ")
+#returb a sobg that has a name and artist_name from inside of the filename
+  info = filename.split(" - ")
 
 song = self.new 
 song.name = info[1].chomp(".mp3")
@@ -62,5 +55,9 @@ def self.destroy_all
   @@all.clear 
 end 
 
+def save
+  self.class.all << self
+  self
+end
 
 end
